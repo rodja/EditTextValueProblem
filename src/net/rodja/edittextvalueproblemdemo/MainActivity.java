5c8dde361b5b4940997e4236ae58b5aa8b2b4de1
Rodja Trappe <mail@rodja.net>
2013-12-06 11:10:10 +0100

    introduced list view to verify the expected behavior

--- src/net/rodja/edittextvalueproblemdemo/MainActivity.java
+++ src/net/rodja/edittextvalueproblemdemo/MainActivity.java
@@ -8,6 +8,9 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
@@ -35,17 +38,26 @@ public class MainActivity extends Activity {
 
         new Thread() {
             public void run() {
+                sleep(1);
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        simulateUserInput((ViewGroup) findViewById(R.id.fragment));
+                    }
+                });
 
                 sleep(2);
 
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
+                        simulateUserInput((ViewGroup) findViewById(R.id.fragment));
                         getFragmentManager().beginTransaction().detach(fragment).commit();
                     }
                 });
 
-                sleep(1);
+                sleep(2);
 
                 runOnUiThread(new Runnable() {
                     @Override
@@ -65,12 +77,55 @@ public class MainActivity extends Activity {
         }.start();
     }
 
+    int input = 0;
+
+    private void simulateUserInput(ViewGroup v) {
+        for (int i = 0; i < v.getChildCount(); i++) {
+            if (v.getChildAt(i) instanceof ViewGroup)
+                simulateUserInput((ViewGroup) v.getChildAt(i));
+            else if (v.getChildAt(i) instanceof EditText) {
+                ((EditText) v.getChildAt(i)).setText("input #" + input++);
+            }
+
+        }
+    }
+
     public static class DemoFragment extends Fragment {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
             Log.v("DemoFragment", "onCreateView");
-            return inflater.inflate(R.layout.fragment_view, container, false);
+
+            View content = inflater.inflate(R.layout.fragment_view, container, false);
+            ListView list = (ListView) content.findViewById(R.id.list);
+            list.setAdapter(new ItemsAdapter());
+            return content;
         }
+
+        private class ItemsAdapter extends ArrayAdapter<String> {
+            public ItemsAdapter() {
+                super(getActivity(), R.layout.editable_item);
+            }
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                LayoutInflater inflater = getActivity().getLayoutInflater();
+                if (convertView == null) {
+                    convertView = inflater.inflate(R.layout.editable_item, null, false);
+                } else {
+                }
+                EditText editText = (EditText) convertView.findViewById(R.id.editText);
+                editText.setText("position: " + position);
+
+                return convertView;
+            }
+
+            @Override
+            public int getCount() {
+                return 5;
+            }
+        }
+
     }
+
 }
